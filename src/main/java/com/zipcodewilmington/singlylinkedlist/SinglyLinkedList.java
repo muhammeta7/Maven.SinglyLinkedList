bfc112a252486c32ffef2625b03c867bb9fdf95f
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-06 21:18:45 -0500

    almost complete besides sort and extra feautures

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,130 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<E extends Comparable<E>> {
+
+    private Node<E> head = null;
+    private Integer size = 0;
+
+    // add -- add an element to the list
+    public void add(E element) {
+        if (head == null) {
+            head = new Node<E>(element);
+        } else {
+            Node<E> nextNode = head;
+            while (nextNode.next != null) {
+                nextNode = nextNode.next;
+            }
+            nextNode.next = new Node<E>(element);
+        }
+        size++;
+    }
+
+    // remove -- remove an element (specified by numeric index) from the list
+    public void remove(E element) throws Exception {
+        int index = find(element);
+        if (index == -1) {
+            throw new Exception("Element does not exist in list!");
+        }
+        if (index == 0) {
+            head = head.next;
+            size--;
+        } else {
+            Node<E> previousNode = head;
+            for (int i = 1; i < index; i++) {
+                previousNode = previousNode.next;
+            }
+            previousNode.setNext(previousNode.next.next);
+            size--;
+        }
+    }
+
+    // - contains -- returns true if the element is in the list, false otherwise
+    public boolean contains(E element) throws Exception {
+        for (int i = 0; i < size; i++) {
+            if (element.equals(get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // - find -- returns the element's index if it is in the list, -1 otherwise
+    public int find(E element) throws Exception {
+        for (int i = 0; i < size; i++) {
+            if (element.equals(get(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // - get -- returns the element at the specified index
+    public E get(int index) throws Exception {
+        if (index < 0 || index >= size) {
+            throw new Exception("Invalid index.");
+        }
+        Node<E> nextNode = head;
+        for (int i = 1; i <= index; i++) {
+            nextNode = nextNode.next;
+        }
+        return  nextNode.getElement();
+    }
+
+    // - size -- returns the current size of the list
+    public int size() {
+        return size;
+    }
+
+    // - copy -- returns a new linked list containing the same values
+    public SinglyLinkedList<E> copy() {
+        SinglyLinkedList<E> returnList = new SinglyLinkedList<>();
+        Node<E> nodeToCopy = head;
+        for (int i = 0; i < size; i++) {
+            returnList.add(nodeToCopy.element);
+            nodeToCopy = nodeToCopy.next;
+        }
+        return returnList;
+    }
+
+    // - sort -- sorts the list using your algorithm of choice.
+   
+
+
+
+    public Node<E> getHead() {
+        return head;
+    }
+
+    static class Node<E extends Comparable<E>> {
+
+        private Node<E> next;
+        private E element;
+
+        public Node(E element) {
+            this.element = element;
+            this.next = null;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+    }
+
 }
+
+
+
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,299 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedListTest {
+public class SinglyLinkedListTest<E> {
+
+    SinglyLinkedList<String> linkedList;
+    SinglyLinkedList<Integer> intList;
+
+    @Before
+    public void setup() {
+        linkedList = new SinglyLinkedList();
+        intList = new SinglyLinkedList<>();
+    }
+
+    // Add Tests
+    //=====================================================================================
+    @Test
+    public void addTest() throws Exception {
+        // Given
+        String expected1 = "first";
+        String expected2 = "second";
+
+        // When
+        linkedList.add(expected1);
+        linkedList.add(expected2);
+        String actual1 = linkedList.get(0);
+        String actual2 = linkedList.get(1);
+
+        // Then
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+    }
+
+    // Remove Tests
+    //=====================================================================================
+    @Test
+    public void removeFirstElementTest() throws Exception {
+        // Given
+        linkedList.add("yo");
+        linkedList.add("bro");
+        String expectedHead = "bro";
+        int expectedSize = 1;
+        int expectedFindYo = -1;
+        int expectedFindBro = 0;
+
+        // When
+        linkedList.remove("yo");
+        String actualHead = linkedList.getHead().getElement();
+        int actualSize = linkedList.size();
+        int actualFindYo = linkedList.find("yo");
+        int actualFindBro = linkedList.find("bro");
+
+        // Then
+        Assert.assertEquals(expectedHead, actualHead);
+        Assert.assertEquals(expectedSize, actualSize);
+        Assert.assertEquals(expectedFindYo, actualFindYo);
+        Assert.assertEquals(expectedFindBro, actualFindBro);
+    }
+
+    @Test
+    public void removeLastElementTest() throws Exception {
+        // Given
+        intList.add(1);
+        intList.add(2);
+
+        int expectedSize = 1;
+        int expectedFind3 = -1;
+
+        // When
+        intList.remove(2);
+        int actualSize = intList.size();
+        int actualFind3 = intList.find(2);
+
+        // Then
+        Assert.assertEquals(expectedSize, actualSize);
+        Assert.assertEquals(expectedFind3, actualFind3);
+        Assert.assertNull(intList.getHead().getNext());
+    }
+
+    @Test
+    public void removeMiddleElementTest() throws Exception {
+        // Given
+        linkedList.add("first");
+        linkedList.add("middle");
+        linkedList.add("last");
+        String expectedHeadNext = "last";
+        int expectedSize = 2;
+
+        // When
+        linkedList.remove("middle");
+        String actualHeadNext = linkedList.getHead().getNext().getElement();
+        int actualSize = linkedList.size();
+
+        // Then
+        Assert.assertEquals(expectedHeadNext, actualHeadNext);
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+
+    @Test(expected = Exception.class)
+    public void removeFromEmptyListTest() throws Exception {
+        // When
+        linkedList.remove("meFromClass");
+    }
+
+    @Test(expected = Exception.class)
+    public void removeNotPresentElementTest() throws Exception {
+        // Given
+        linkedList.add("bananas");
+
+        // When
+        linkedList.remove("apples");
+    }
+
+    // Contains Tests
+    //=====================================================================================
+    @Test
+    public void emptyContainsTest() throws Exception {
+        // When
+        boolean actual = linkedList.contains("false");
+
+        // Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void nonEmptyContainsTest() throws Exception {
+        // Given
+        String expectedTrue = "true";
+        String expectedFalse = "false";
+        linkedList.add("these exist");
+        linkedList.add(expectedTrue);
+
+        // When
+        boolean actualTrue = linkedList.contains(expectedTrue);
+        boolean actualFalse = linkedList.contains(expectedFalse);
+
+        // Then
+        Assert.assertTrue(actualTrue);
+        Assert.assertFalse(actualFalse);
+    }
+
+    // Find Tests
+    //=====================================================================================
+    @Test
+    public void emptyFindTest() throws Exception {
+        // Given
+        int expected = -1;
+
+        // When
+        int actual = linkedList.find("not here");
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void containsElementFindTest() throws Exception {
+        // Given
+        intList.add(10);
+        int expected1 = 0;
+        intList.add(20);
+        int expected2 = 1;
+
+        // When
+        int actual1 = intList.find(10);
+        int actual2 = intList.find(20);
+
+        // Then
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+    }
+
+    @Test
+    public void doesNotContainElementFindTest() throws Exception {
+        // Given
+        linkedList.add("these");
+        linkedList.add("things");
+        int expected = -1;
+
+        // When
+        int actual = linkedList.find("ghost");
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    // Get Tests
+    //=====================================================================================
+    @Test(expected = Exception.class)
+    public void emptyListGetTest() throws Exception {
+        // Given
+        linkedList.get(0);
+    }
+
+    @Test
+    public void nonEmptyListGetTest() throws Exception {
+        // Given
+        Integer expected1 = 10;
+        Integer expected2 = 20;
+        intList.add(expected1);
+        intList.add(expected2);
+
+        // When
+        Integer actual1 = intList.get(0);
+        Integer actual2 = intList.get(1);
+
+        // Then
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+    }
+
+    @Test(expected = Exception.class)
+    public void invalidIndexGetTest() throws Exception {
+        // Given
+        intList.add(10);
+        intList.add(3);
+
+        // When
+        linkedList.get(2);
+    }
+
+    // Size
+    //=====================================================================================
+    @Test
+    public void emptySizeTest() {
+        // Given
+        int expected = 0;
+
+        // When
+        int actual = linkedList.size();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void nonEmptySizeTest() {
+        // Given
+        linkedList.add("uno");
+        linkedList.add("dos");
+        int expected = 2;
+
+        // When
+        int actual = linkedList.size();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    // Copy Tests
+    //=====================================================================================
+    @Test
+    public void copyEmptyTest(){
+        // Given
+        int expectedSize = 0;
+
+        // When
+        SinglyLinkedList<String> copy = linkedList.copy();
+        int actualSize = copy.size();
+
+        // Then
+        Assert.assertEquals(expectedSize, actualSize);
+        Assert.assertNull(copy.getHead());
+        Assert.assertNotEquals(linkedList, copy);
+    }
+
+    @Test
+    public void copyTest(){
+        // Given
+        String expected1 = "make";
+        String expected2 = "copy";
+        linkedList.add(expected1);
+        linkedList.add(expected2);
+        int expectedSize = 2;
+
+        // When
+        SinglyLinkedList<String> copy = linkedList.copy();
+        int actualSize = copy.size();
+        String actual1 = linkedList.getHead().getElement();
+        String actual2 = linkedList.getHead().getNext().getElement();
+
+        // Then
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
 }
+
